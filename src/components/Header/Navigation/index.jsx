143789f2fc4bab1844157abb7675f129c1d881d9
johntvale <johntvale@gmail.com>
2022-04-19 18:45:56 -0400

    fix: correçao de bugs no menu navigation

--- src/components/Header/Navigation/index.jsx
+++ src/components/Header/Navigation/index.jsx
@@ -1,35 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import menuIcon from '../../../assets/icons/menu-icon.png';
 import logoIcon from '../../../assets/icons/logo-icon.png';
-import { changeHomeBtnPosition, setMenuByWindowWidth } from '../../../helpers';
+import {
+  hideMenuIfScrolling,
+  setHomeBtnPosition,
+  setInitialNavigationMode,
+  setNavigationMode
+} from '../../../helpers';
 import './style.css';
 
 export default function Navigation() {
-  const [homeBtnPosition, setHomeBtnPosition] = useState('logo-icon');
+  const [homeBtnClassName, setHomeBtnClassName] = useState('logo-icon');
   const [showMenuList, setShowMenuList] = useState(false);
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
 
   useEffect(() => {
-    // detect scrolling to set home button's position and hide the menu list
-    window.addEventListener('scroll', (event) =>
-      changeHomeBtnPosition(event, setHomeBtnPosition, setShowMenuList)
-    );
-  }, [homeBtnPosition]);
-
-  useEffect(() => {
-    // detect window size
-    window.addEventListener('resize', (event) => setMenuByWindowWidth(event, setShowMenuList));
+    // configuring navigation using scrolling info
+    const positionScrollBeforeEvent = window.screenY;
+    window.addEventListener('scroll', (event) => {
+      setHomeBtnPosition(event, setHomeBtnClassName);
+      hideMenuIfScrolling(event, positionScrollBeforeEvent, setShowMenuList);
+    });
   }, [showMenuList]);
 
-  function handlerMenu() {
+  useEffect(() => {
+    // configuring navigation using initial screen width
+    setInitialNavigationMode(screenSize, setShowMenuList);
+  }, []);
+
+  useEffect(() => {
+    // configuring navigation using current screen width
+    setNavigationMode(screenSize, setScreenSize, setShowMenuList);
+  }, [screenSize]);
+
+  function toggleMenuHandler() {
     setShowMenuList(!showMenuList);
   }
 
   return (
     <nav className="navbar">
       <a href="#">
-        <img className={`${homeBtnPosition}`} src={logoIcon} alt="Logo ISS Tracker" />
+        <img className={`${homeBtnClassName}`} src={logoIcon} alt="Logo ISS Tracker" />
       </a>
-      <button className="menu-button" onClick={handlerMenu}>
+      <button className="menu-button" onClick={toggleMenuHandler}>
         <img className="menu-icon" src={menuIcon} />
       </button>
       {showMenuList && (
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,15 +1,32 @@
-export const changeHomeBtnPosition = (event, setHomeBtnPosition, setShowMenuList) => {
+export const setHomeBtnPosition = (event, setHomeBtnClassName) => {
+  // set button home on bottom with page down event
   const detectedScroll = event.path[1].scrollY;
   if (detectedScroll > 52) {
-    setHomeBtnPosition('logo-icon-home');
+    setHomeBtnClassName('logo-icon-home');
   } else {
-    setHomeBtnPosition('logo-icon');
+    setHomeBtnClassName('logo-icon');
   }
-  setShowMenuList(false); // desativa o menu caso esteja aberto
 };
 
-export const setMenuByWindowWidth = (event, setShowMenuList) => {
-  const deviceWidth = event.target.window.innerWidth;
-  if (deviceWidth > 999) setShowMenuList(true);
+export const setInitialNavigationMode = (screenSize, setShowMenuList) => {
+  // set the initial navigation mode to Menu List if the screen width were bigger than 999px or keep it on Dropdown mode if were not
+  if (screenSize > 999) setShowMenuList(true);
   else setShowMenuList(false);
+};
+
+export const setNavigationMode = (screenSize, setScreenSize, setShowMenuList) => {
+  // update the navigation mode if there were change on screen width
+  window.addEventListener('resize', (event) => {
+    const currentScreenSize = event.target.window.innerWidth;
+    if (screenSize !== currentScreenSize && currentScreenSize > 999) setShowMenuList(true);
+    else setShowMenuList(false);
+    setScreenSize(currentScreenSize);
+  });
+};
+
+export const hideMenuIfScrolling = (event, positionScrollBeforeEvent, setShowMenuList) => {
+  // hide the menu if were detected scrolling with the menu open
+  const detectedScroll = event.path[1].scrollY;
+  if (window.innerWidth < 1000 && positionScrollBeforeEvent !== detectedScroll)
+    setShowMenuList(false);
 };
