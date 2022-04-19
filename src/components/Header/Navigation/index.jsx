a969331ed8b738fb4c8d3076ed83f04775ac29f4
johntvale <johntvale@gmail.com>
2022-04-19 16:18:03 -0400

    feat: Adiciona funcionamento ao botao Menu, incluindo responsividade

--- src/components/Header/Navigation/index.jsx
+++ src/components/Header/Navigation/index.jsx
@@ -1,36 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import menuIcon from '../../../assets/icons/menu-icon.png';
 import logoIcon from '../../../assets/icons/logo-icon.png';
-import { changeHomeBtnPosition } from '../../../helpers';
+import { changeHomeBtnPosition, setMenuByWindowWidth } from '../../../helpers';
 import './style.css';
 
 export default function Navigation() {
   const [homeBtnPosition, setHomeBtnPosition] = useState('logo-icon');
+  const [showMenuList, setShowMenuList] = useState(false);
 
   useEffect(() => {
-    window.addEventListener('scroll', (event) => changeHomeBtnPosition(event, setHomeBtnPosition));
+    // detect scrolling to set home button's position and hide the menu list
+    window.addEventListener('scroll', (event) =>
+      changeHomeBtnPosition(event, setHomeBtnPosition, setShowMenuList)
+    );
   }, [homeBtnPosition]);
+
+  useEffect(() => {
+    // detect window size
+    window.addEventListener('resize', (event) => setMenuByWindowWidth(event, setShowMenuList));
+  }, [showMenuList]);
+
+  function handlerMenu() {
+    setShowMenuList(!showMenuList);
+  }
 
   return (
     <nav className="navbar">
       <a href="#">
         <img className={`${homeBtnPosition}`} src={logoIcon} alt="Logo ISS Tracker" />
       </a>
-      <img className="menu-icon" src={menuIcon} />
-      <ul>
-        <li>
-          <a href="#">home</a>
-        </li>
-        <li>
-          <a href="#location-section">location</a>
-        </li>
-        <li>
-          <a href="#location-crew">crew</a>
-        </li>
-        <li>
-          <a href="#location-missions">missions</a>
-        </li>
-      </ul>
+      <button className="menu-button" onClick={handlerMenu}>
+        <img className="menu-icon" src={menuIcon} />
+      </button>
+      {showMenuList && (
+        <ul>
+          <li>
+            <a href="#">home</a>
+          </li>
+          <li>
+            <a href="#location-section">location</a>
+          </li>
+          <li>
+            <a href="#location-crew">crew</a>
+          </li>
+          <li>
+            <a href="#location-missions">missions</a>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,8 +1,15 @@
-export function changeHomeBtnPosition(event, setHomeBtnPosition) {
+export const changeHomeBtnPosition = (event, setHomeBtnPosition, setShowMenuList) => {
   const detectedScroll = event.path[1].scrollY;
   if (detectedScroll > 52) {
     setHomeBtnPosition('logo-icon-home');
   } else {
     setHomeBtnPosition('logo-icon');
   }
-}
+  setShowMenuList(false); // desativa o menu caso esteja aberto
+};
+
+export const setMenuByWindowWidth = (event, setShowMenuList) => {
+  const deviceWidth = event.target.window.innerWidth;
+  if (deviceWidth > 999) setShowMenuList(true);
+  else setShowMenuList(false);
+};
