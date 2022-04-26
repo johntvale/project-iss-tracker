1eb15ca034ee76ce85fa4c97850be4c0abbc8a87
johntvale <johntvale@gmail.com>
2022-04-26 15:43:33 -0400

    fix: pequenas correçoes na estilizaçao

--- src/components/Header/TextHero/index.jsx
+++ src/components/Header/TextHero/index.jsx
@@ -6,7 +6,7 @@ export default function TextHero() {
     <div className="text-hero">
       <h1 className="title">ISS</h1>
       <h1 className="title">Tracker</h1>
-      <h2 className="subtitle">The Internacional Space Station just an app away.</h2>
+      <h2 className="subtitle">The International Space Station just a click away.</h2>
     </div>
   );
 }
