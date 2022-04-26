673629d7dcea8fc8029d4a609370122e13081dc2
johntvale <johntvale@gmail.com>
2022-04-26 15:42:51 -0400

    feat: Implementaçao do componente Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Location from './components/Sections/Location';
 import Crew from './components/Sections/Crew';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Location />
       <Crew />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+export default function Footer() {
+  return (
+    <footer>
+      <h3>
+        <span>ISS Tracker</span> - The International Space Station just a click away.
+      </h3>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#location-section">Location</a>
+        <a href="#crew-section">Crew</a>
+        <a href="#fun-facts">Fun Facts</a>
+      </nav>
+      <p>
+        Website developed by: <a href="https://www.linkedin.com/in/jtvale/">John Torres</a>.
+        Repository: <a href="https://github.com/johntvale/project-iss-tracker">ISS Tracker</a>
+      </p>
+    </footer>
+  );
+}
