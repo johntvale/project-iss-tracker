73d07ca77527d78410d8608b5b80ec3c18abd1a0
johntvale <johntvale@gmail.com>
2022-04-29 13:24:26 -0400

    fix: adicionando propriedade playsinline ao background video do header

--- src/components/Header/BackgroundVideo/index.jsx
+++ src/components/Header/BackgroundVideo/index.jsx
@@ -3,5 +3,5 @@ import video from '../../../assets/videos/iss-time-lapse.mp4';
 import './style.css';
 
 export default function BackgroundVideo() {
-  return <video src={video} autoPlay muted loop className="background-video" />;
+  return <video src={video} autoPlay muted loop playsInline className="background-video" />;
 }
